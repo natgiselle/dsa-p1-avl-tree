f8cbfcf1407dd00bc982ba402f9be3262dcd04f4
natalie <natgiselle29@gmail.com>
2026-09-25 19:33:13 -0400

    implements print student names func

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,12 @@
 using namespace std;
 
 int main(){
-	
+	void printStudentNames(const vector<string>& names) {
+		for (int i = 0; i < names.size(); i++) {
+			cout << names[i]; // prints the name of each student
+			if (1 + i > names.size()) cout << ", ";
+		}
+		cout << endl;
+	}
 	return 0;
 }
