fa004484958fa1727b30258cb66013aaae1bd619
natalie <natgiselle29@gmail.com>
2026-09-25 19:35:47 -0400

    declares the function outside of the main method

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,17 @@
 #include "avl_tree.h"
-#pragma once
-#include <iostream>
-#include <vector>
-#include <string>
-#include <regex>
 
 using namespace std;
 
-int main(){
-	void printStudentNames(const vector<string>& names) {
-		for (int i = 0; i < names.size(); i++) {
-			cout << names[i]; // prints the name of each student
-			if (1 + i > names.size()) cout << ", ";
-		}
-		cout << endl;
+void printStudentNames(const vector<string>& names) {
+	for (int i = 0; i < names.size(); i++) {
+		cout << names[i]; // prints the name of each student
+		if (1 + i > names.size()) cout << ", ";
 	}
+	cout << endl;
+}
+
+
+int main() {
+	
 	return 0;
 }
