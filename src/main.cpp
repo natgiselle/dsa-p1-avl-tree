00e176b78efb42ac219ec91795612de19fc5ad0c
natalie <natgiselle29@gmail.com>
2026-09-25 20:08:33 -0400

    fixes size_t index

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,55 @@
-#include "avl_tree.h"
 
+#include <sstream>
+#include <string>
+#include "avl_tree.h"
 using namespace std;
 
 void printStudentNames(const vector<string>& names) {
 	for (int i = 0; i < names.size(); i++) {
 		cout << names[i]; // prints the name of each student
-		if (1 + i > names.size()) cout << ", ";
+		if (i + 1 < names.size()) cout << ", ";
 	}
 	cout << endl;
 }
 
 
 int main() {
-	
+	AVLTree tree;
+	// get the lines from the user inpt and m
+	string lineCt;
+	getline(cin, lineCt);
+	int lineTotal = stoi(lineCt);
+
+	for(size_t i = 0; i < lineTotal; i++) {
+		string line;
+		getline(cin, line);
+
+		istringstream in(line);
+		string command;
+		in >> command;
+
+		if (command == "insert") {
+			string discard, name, strId;
+			getline(in, discard, '"');
+			getline(in, name, '"');
+			in >> strId;
+			if (tree.insert(name, strId)) cout << "succesful";
+			else {
+			cout << "unsuccessful";
+			}
+		}
+
+		else if (command == "remove") {
+			string discard, strId;
+			getline(in, discard, '"');
+			getline(in, strId, '"');
+			in >> strId;
+			if(tree.remove(strId)) cout << "successful";
+			else {
+				cout << "unsuccessful";
+			}
+		}
+	}
+
 	return 0;
 }
